a23a7d11417b196e791605913f9f2c3d40802b96
MiguelBraga2 <mgdsbraga@gmail.com>
2024-05-27 16:09:57 +0100

    Initial commit

---
+++ oportuniza-backend/src/main/java/org/oportuniza/oportunizabackend/OportunizaBackendApplication.java
@@ -0,0 +1,13 @@
+package org.oportuniza.oportunizabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OportunizaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OportunizaBackendApplication.class, args);
+    }
+
+}
---
+++ oportuniza-backend/src/test/java/org/oportuniza/oportunizabackend/OportunizaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.oportuniza.oportunizabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OportunizaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
