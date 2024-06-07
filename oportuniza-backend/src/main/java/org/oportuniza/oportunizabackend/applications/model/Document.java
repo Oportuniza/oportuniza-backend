906a877ec0f74124d36690e6526e2d51f06ebdd5
jmartins9 <joaoantonioredondo1994@gmail.com>
2024-06-07 16:07:43 +0100

    Correction in applications

--- oportuniza-backend/src/main/java/org/oportuniza/oportunizabackend/applications/model/Document.java
+++ oportuniza-backend/src/main/java/org/oportuniza/oportunizabackend/applications/model/Document.java
@@ -1,20 +1,23 @@
 package org.oportuniza.oportunizabackend.applications.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
 @Table(name = "documents")
 public class Document {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    private long id;
 
     private String url;
 
     @ManyToOne
-    @JoinColumn(name = "application_id")
+    @JoinColumn(name = "application_id", nullable = false)
     private Application application;
 }
