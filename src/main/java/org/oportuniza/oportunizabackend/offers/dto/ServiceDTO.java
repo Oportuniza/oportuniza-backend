310b4217f44d92037b0e55387a0dfc3a43fade67
jmartins9 <joaoantonioredondo1994@gmail.com>
2024-06-13 17:06:52 +0100

    timestamps

--- src/main/java/org/oportuniza/oportunizabackend/offers/dto/ServiceDTO.java
+++ src/main/java/org/oportuniza/oportunizabackend/offers/dto/ServiceDTO.java
@@ -4,13 +4,24 @@ package org.oportuniza.oportunizabackend.offers.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Date;
+
 @Getter
 @Setter
 public class ServiceDTO extends OfferDTO {
     double price;
 
+    public ServiceDTO(long id, String title, String description, boolean negotiable, Date createdAt, double price) {
+        super(id, title, description, negotiable, createdAt);
+        this.price = price;
+    }
+
     public ServiceDTO(long id, String title, String description, boolean negotiable, double price) {
         super(id, title, description, negotiable);
         this.price = price;
     }
+
+    public ServiceDTO() {
+    }
+
 }
